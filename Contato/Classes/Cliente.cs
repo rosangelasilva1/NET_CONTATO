10cd7ff7712b5e1b0ebc13d750603fd271a1446a
Rosangela Silva <roliveirasv@gmail.com>
2019-07-26 02:21:34 +0000

    Exemplo de propriedade na classe cliente - snippet propfull

--- Contato/Classes/Cliente.cs
+++ Contato/Classes/Cliente.cs
@@ -9,7 +9,7 @@ namespace Contato.Classes
     public partial class Cliente
     {
 
-        public  int Codigo { get; set; }
+        //public  int Codigo { get; set; }
         public string Nome { get; set; }
         public int Tipo { get; set; }
         public DateTime DataCadastro { get; set; }
@@ -17,8 +17,27 @@ namespace Contato.Classes
         //Cria uma lista de contatos
         public List<Contato> contatos { get; set; }
 
+        //Para criar uma propriedade com métodos get e set para serem acessados através de variáveis privadas
+        //Você pode criar através do snippet propful <TAB> <TAB>
+        //É muito utilizada para fazer valodações. Exemplo:
+        private int _codigo;
+
+        public int Codigo
+        {
+            get { return _codigo; }
+
+            set {
+
+                    if (_codigo < 0 )
+                    {
+                    _codigo = 0; 
+                    }
+                    _codigo = value;
+                }
+        }
 
 
 
-    }
+
+    }   
 }
